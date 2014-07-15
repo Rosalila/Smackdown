33cf0c98e40bc6546d0dc3bf4b2a2630c6ec154d
Turupawn <turupawn@userr>
2014-07-14 23:55:46 -0600

    switches

--- db/schema.rb
+++ db/schema.rb
@@ -13,12 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140715020529) do
 
-  create_table "game_modes", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "games", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
