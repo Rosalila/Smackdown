7671a20b4f0babf5216b3b99d45ed721039d5968
Turupawn <ahmed.hn.43@gmail.com>
2014-08-24 23:29:49 +0000

    annotations done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
+
+  def getCurrentTimeZone
+    return "America/Tegucigalpa"
+  end
+
   def userIsPlayingGame (user_id,game_id)
     upg = UserPlayingGame.where(:user_id=>user_id, :game_id=>game_id)[0]
     if upg == nil
@@ -82,6 +87,55 @@ module ApplicationHelper
     end
 
     return {'user1_rechazadas' => user1_rechazadas, 'user2_rechazadas' => user2_rechazadas, 'user1_ganadas' => user1_ganadas, 'user2_ganadas' => user2_ganadas, 'conflictos'=>conflictos, 'pendientes'=>pendientes, 'errores' => errores, 'totales'=>totales}
+  end
+
+  def annotationChartScript player1, player2
+    str = "<script type=\"text/javascript\" src=\"https://www.google.com/jsapi?autoload={'modules':[{'name':'visualization','version':'1','packages':['annotationchart']}]}\"></script>"+
+    "<script type='text/javascript'>"+
+    "  google.load('visualization', '1', {'packages':['annotationchart']});"+
+    "  google.setOnLoadCallback(drawChart);"+
+    "  function drawChart() {"+
+    "    var data = new google.visualization.DataTable();"+
+    "    data.addColumn('date', 'Fecha');"+
+    "    data.addColumn('number', 'Diferencia');"+
+    "    data.addColumn('string', 'Evento');"+
+    "    data.addColumn('string', 'Resultado');"+
+    "    data.addRows(["
+    difference = 0
+    text_difference= ""
+    Smackdown.where(player1_id: [player1.id,player2.id], player2_id: [player1.id,player2.id], judge1_winner_id: [player1.id,player2.id], judge2_winner_id: [player1.id,player2.id]).each do |smackdown|
+      result = ""
+      if smackdown.judge1_winner_id == player1.id && smackdown.judge2_winner_id == player1.id
+        difference += 1
+        result = "Ganador: " + player1.name
+      elsif smackdown.judge1_winner_id == player2.id && smackdown.judge2_winner_id == player2.id
+        difference -= 1
+        result = "Ganador: " + player2.name
+      end
+
+      text_difference= "Empate"
+      if difference>0
+        text_difference = "(diferencia: +"+(difference.to_s)+" para "+player1.name+")"
+      elsif difference<0
+        text_difference = "+"+((-difference).to_s)+" para "+player2.name
+      end
+
+      str+="      [new Date("+ (smackdown.created_at.year.to_s) +", "+ ((smackdown.created_at.month-1).to_s) +", "+ (smackdown.created_at.day.to_s) +"), "+ (difference.to_s) + ", \"" + result + "\",  \"" + text_difference + "\"], "
+    end
+    today = Time.now.in_time_zone(getCurrentTimeZone)
+    str+="      [new Date(" + (today.year).to_s + ", " + (today.month-1).to_s + ", " + (today.day).to_s + "), " + (difference.to_s) + ",'Actualidad', \"" + text_difference + "\"]"
+    str+="    ]);"+
+    "     var chart = new google.visualization.AnnotationChart(document.getElementById('annotation_div'));"+
+    "     var options = {"+
+    "       displayAnnotations: true"+
+    "     };"+
+    "    chart.draw(data, options);"+
+    "  }"+
+    "</script>"+
+    "<body>"+
+    "  <div id='annotation_div' style='width: 900px; height: 500px;'></div>"+
+    "</body>"
+    return str
   end
 
 end
