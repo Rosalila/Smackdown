2f94970a3aa6676855856ad4315635960a92b4f1
Turupawn <ahmed.hn.43@gmail.com>
2015-02-24 23:50:07 -0600

    profiles fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  file = File.open("/home/rails/facebook.secretkey", "rb")
+  #file = File.open("/home/rails/facebook.secretkey", "rb")
+  file = File.open("/home/turupawn/facebook.secretkey", "rb")
   provider :facebook, '672588699490076', file.read.chomp
 end
