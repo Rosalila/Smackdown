96dda298e89b89d7a228d5e82173f4630436f325
Turupawn <ahmed.hn.43@gmail.com>
2015-06-10 15:20:39 -0600

    development/production disctinction (I hope this works)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   private
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    #User.first
+    if Rails.env.production?
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    else
+      User.first
+    end
   end
   helper_method :current_user
 end
