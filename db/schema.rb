b5e744c6f0337dd82f97842cd5120920022c9c96
Turupawn <ahmed.hn.43@gmail.com>
2015-07-17 00:54:58 +0000

    woops

--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,12 @@ ActiveRecord::Schema.define(version: 20150713211037) do
     t.datetime "updated_at"
   end
 
+  create_table "game_modes", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "games", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
