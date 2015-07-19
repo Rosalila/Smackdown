ab84f1fb5449f45ebbca9cb3b089f1856f786e26
Turupawn <ahmed.hn.43@gmail.com>
2015-07-19 15:59:13 -0600

    tricks and treats

--- app/controllers/smackdown_creator_controller.rb
+++ app/controllers/smackdown_creator_controller.rb
@@ -1,7 +1,11 @@
 class SmackdownCreatorController < ApplicationController
   def select_user
     @like_param = params["like_param"]
-    @users=User.where("name LIKE ?" , "%#{@like_param}%")
+    if @like_param=="" || @like_param == nil
+      @users=[]
+    else
+      @users=User.where("name LIKE ?" , "%#{@like_param}%")
+    end
   end
 
   def select_game
@@ -12,7 +16,11 @@ class SmackdownCreatorController < ApplicationController
     @user = User.find(params[:user_id])
     @game = Game.find(params[:game_id])
     @like_param = params["like_param"]
-    @judges=User.where("name LIKE ?" , "%#{@like_param}%")
+    if @like_param=="" || @like_param == nil
+      @judges=[]
+    else
+      @judges=User.where("name LIKE ?" , "%#{@like_param}%")
+    end
   end
 
   def select_rules
