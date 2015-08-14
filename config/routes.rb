9803fdb2182512ce6619d96945adedead27e898c
Turupawn <ahmed.hn.43@gmail.com>
2015-08-13 19:07:42 -0600

    bye bye ajax

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Myapp::Application.routes.draw do
   get '/communities/join', to: 'communities#join', as: 'join_community'
   get '/communities/leave', to: 'communities#leave', as: 'leave_community'
   get '/communities/manage_users', to: 'communities#manage_users', as: 'community_manage_users'
+  get '/user_playing_games/play_game', to: 'user_playing_games#play_game', as: 'user_playing_games_play_game'
+  get '/user_playing_games/stop_playing_game', to: 'user_playing_games#stop_playing_game', as: 'user_playing_games_stop_playing_game'
+  #get '/home/set_rules', to: 'home#set_rules', as: 'home_set_rules'
+  match '/home/set_rules' => 'home#set_rules', via: :post
 
   resources :user_in_communities
 
