f77915926ebb4e6ded454f0b7cc8dec097208e78
Turupawn <ahmed.hn.43@gmail.com>
2016-02-05 17:36:12 -0600

    testa

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
   has_many :player_rules
 
   def self.from_omniauth(auth)
-    #if auth.provider == "facebook"
+    if auth.provider == "facebook"
       where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
         user.provider = auth.provider
         user.uid = auth.uid
@@ -20,12 +20,12 @@ class User < ActiveRecord::Base
         user.oauth_expires_at = Time.at(auth.credentials.expires_at)
         user.save!
       end
-    #end
-    #if auth.provider == "steam"
+    end
+    if auth.provider == "steam"
       #if current_user
       #  current_user.steamid = "testa"
       #end
-    #end
+    end
   end
 
   def hasRule rule_id
