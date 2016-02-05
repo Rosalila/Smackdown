f66c409191092a354774ed9fb367690c000ef987
Turupawn <ahmed.hn.43@gmail.com>
2016-02-05 17:23:51 -0600

    test

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -8,5 +8,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     file = File.open("/home/turupawn/facebook.secretkey", "rb")
   end
   provider :facebook, '672588699490076', file.read.chomp
-  #provider :steam, '63B42B959BC86C52FCA00AD37AFCC81C'
+  provider :steam, '63B42B959BC86C52FCA00AD37AFCC81C'
 end
